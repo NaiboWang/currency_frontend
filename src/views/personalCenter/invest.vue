41b6cf9992ed04f4ca0914f46e2c50504e3c0cda
NaiboWang-Alienware <naibowang@u.nus.edu>
2021-12-24 01:30:31 +0800

    developing...

--- src/views/personalCenter/invest.vue
+++ src/views/personalCenter/invest.vue
@@ -32,11 +32,11 @@
                 <div class="row">
 
                   <div class="col-sm-6 form-check form-check-flat form-check-info check-hide">
-                    <div class="check-hide-inner">
-                      <label class="form-check-label">
-                        <input v-model="hideSmall" type="checkbox" class="form-check-input"> Hide Small Balance <i
-                          class="input-helper"></i> </label>
-                    </div>
+<!--                    <div class="check-hide-inner">-->
+<!--                      <label class="form-check-label">-->
+<!--                        <input v-model="hideSmall" type="checkbox" class="form-check-input"> Hide Small Balance <i-->
+<!--                          class="input-helper"></i> </label>-->
+<!--                    </div>-->
                   </div>
 
                   <b-input-group class="col-sm-6 mt-1">
